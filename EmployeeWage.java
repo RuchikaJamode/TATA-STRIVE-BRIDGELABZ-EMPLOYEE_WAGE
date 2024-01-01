4723d3323b58c2776947feaa6c94a25d8d884bef
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-01 17:17:08 +0530

    welcome to Employee Wage computationa program

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,7 +4,7 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("welcome to employeewage computational program");
 	}
 
 }
